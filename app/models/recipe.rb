52057d274b9091361c4a46d3dc67f759498dff3e
Upa005 <upasana05ghosh@gmail.com>
2016-07-30 12:24:39 +0530

    heroku ready

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,7 @@ class Recipe
   	include HTTParty
   	key_value =  ENV['FOOD2FORK_KEY']
     	#hostport = 'www.food2fork.com'
-    	hostport =  'www.food2fork.com' || ENV['FOOD2FORK_SERVER_AND_PORT'] 
+    	hostport =   ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com' 
     	base_uri "http://#{hostport}/api"
 	#base_uri 'http://food2fork.com/api'
 	default_params key: key_value
